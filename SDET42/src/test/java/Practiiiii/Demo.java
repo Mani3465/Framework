a977d4fa81c9e361e366590608069139384c3e0b
Mr <Mr@LAPTOP-AIVEMT1H>
2022-10-17 15:03:44 +0530

    moratu single

---
+++ SDET42/src/test/java/Practiiiii/Demo.java
@@ -0,0 +1,10 @@
+package Practiiiii;
+
+	public class Demo {
+		public static void main(String[] args) {
+			System.out.println("hii");
+			System.out.println("hello");
+			System.out.println("bye");
+		}
+		}
+
